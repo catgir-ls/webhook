139b9a83e11f46337f1d8cb1bf8933a9dee544ec
e991f665b7e62df5a54fdef19053a4e75117b89 <c@catgir.ls>
2023-11-08 01:18:12 +0000

    fix(event/push): fix trunacting

--- events/PushEvent.ts
+++ events/PushEvent.ts
@@ -46,7 +46,7 @@ class PushEvent extends Event {
       ].join("\n"),
       fields: commits.map((commit: PartialCommit) => ({
         name: `\`${commit.id.slice(0, 8)}\``,
-        value: `\`\`\`fix\n${commit.message.slice(0, 50 - 3)}...\n\`\`\``, /** Trunucate commit message at 50 characters */
+        value: `\`\`\`fix\n${commit.message.slice(0, 50 - 3)}${commit.message.length > 50 ? "..." : ""}\n\`\`\``, /** Trunucate commit message at 50 characters */
         inline: commit.message.length < 50
       })),
       ...Webhook.getDefaults(sender)
