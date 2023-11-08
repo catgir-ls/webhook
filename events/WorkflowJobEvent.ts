f55ff3df083654827c359d47b9dadfd25be86d18
af280cf94190a54043e947948a0031ce7ed71dc <rookie2987@00111111.xyz>
2023-11-08 01:06:57 +0100

    Update WorkflowJobEvent.ts
    written on mobile suck my nuts

--- events/WorkflowJobEvent.ts
+++ events/WorkflowJobEvent.ts
@@ -26,10 +26,13 @@ class WorkflowJobEvent extends Event {
     if(action !== "completed") return;
 
     const completed_steps = workflow_job.steps.filter((step: Obj) => step.conclusion === "success").length
+    let description = `>  Workflow in [\`${repository.full_name}\`](https://github.com/${repository.full_name}) has successfully completed **${completed_steps}/${workflow_job.steps.length}** steps`
+    if(completed_steps != workflow_job.steps.length)
+    description = `> Workflow in [\`${repository.full_name}\`](https://github.com/${repository.full_name}) has failed to complete successfully`;
 
     await Webhook.send({
       title: `Pipeline for ${repository.full_name}`,
-      description: `> Workflow in [\`${repository.full_name}\`](https://github.com/${repository.full_name}) has successfully completed **${completed_steps}/${workflow_job.steps.length}** steps`,
+      description,
       fields: [{
         name: `\`Steps\``,
         value: `\`\`\`fix\n${workflow_job.steps.map((step: Obj) => `${step.name.replace(/\*\*\*/g, "registry")} ${step.conclusion === "success" ? "✅" : "❌"}`).join("\n")}\n\`\`\``
