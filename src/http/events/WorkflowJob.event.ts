6142a9e3c297bfa6b7115f7cc773d0431e328da6
e991f665b7e62df5a54fdef19053a4e75117b89 <c@catgir.ls>
2024-12-29 08:02:40 +0000

    feat: added automatic kubernetes container updates

--- src/http/events/WorkflowJob.event.ts
+++ src/http/events/WorkflowJob.event.ts
@@ -8,22 +8,31 @@ import { Event } from "@src/decorators";
 /** Utils */
 import { Logger } from "../../utils/Logger.ts";
 
+/** Config */
+import Config from "@src/config";
+
 /** Types */
 import {
   type Obj,
 
   EventType
 } from "@src/types";
+import { Kubernetes } from "../../utils/Kubernetes.ts";
 
 /** WorkflowJob Event */
 @Event([ EventType.WORKFLOW_JOB ])
 class WorkflowJobEvent {
-  public execute = ({ action, workflow_job, repository, organization }: Obj) => {
+  public execute = async ({ action, workflow_job, repository }: Obj) => {
     if(action !== "completed") return;
 
-    const name = organization?.login ?? repository?.full_name; 
+    Logger.log(`Workflow in ${repository.full_name} has ${workflow_job.conclusion === "success" ? "completed" : "failed"}`);
 
-    Logger.log(`Workflow in ${name} has ${workflow_job.conclusion === "success" ? "completed" : "failed"}`);
+    if(!await Kubernetes.update(
+      Config.get<string>("kubernetes", "namespace"),
+      repository.name
+    )) return Logger.error(`Unable to push update for ${repository.full_name}`);
+
+    Logger.log(`Succesfully pushed update for ${repository.full_name}`);
   }
 }
 
--- src/utils/Kubernetes.ts
+++ src/utils/Kubernetes.ts
@@ -19,6 +19,55 @@ class Kubernetes {
     && existsSync(TOKEN_PATH)
   );
 
+  private static getDeployment = async (
+    namespace: string,
+    repository: string
+  ) => {
+    const response = await fetch(`${this.base_url}/apis/apps/v1/namespaces/${namespace}/deployments`, {
+      headers: {
+        "Authorization": `Bearer ${this.token}`,
+        "Content-Type": "application/json"
+      }
+    }), body = await response.json();
+
+    for(const item of body.items) {
+      const [ container ] = item.spec.template.spec.containers;
+      
+      if(!container || container.image.indexOf(repository) === -1)
+        continue;
+
+      return item;
+    }
+
+    return null;
+  }
+
+  public static update = async (
+    namespace: string,
+    repository: string
+  ): Promise<boolean> => {
+    const deployment = await this.getDeployment(namespace, repository);
+
+    if(!deployment)
+      return false;
+
+    deployment.spec.template.metadata.annotations = {
+      ...deployment.spec.template.metadata.annotations,
+      date: Date.now().toString()
+    };
+
+    const response = await fetch(`${this.base_url}/apis/apps/v1/namespaces/${namespace}/deployments/${deployment.metadata.name}`, {
+      method: "PUT",
+      headers: {
+        "Authorization": `Bearer ${this.token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(deployment)
+    });
+
+    return response.ok;
+  }
+
   public static init = (): boolean => {
     if(!this.isKubernetesEnvironment()) 
       return false;
